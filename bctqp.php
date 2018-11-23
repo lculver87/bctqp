d50ddf2253be2f1744279877f4f36c8453188b3d
lculver87 <liamculver@gmail.com>
2018-11-23 20:13:10 +0000

    Create bctqp.php

---
+++ bctqp.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+Plugin Name: Big Chief Tipis Quote Plugin
+Plugin URI: https://github.com/lculver87/bctquoteplugin
+Description: A WordPress plugin to easily add an FAQs section to your WordPress site.
+Version: 1.0
+Author: Liam Cuver
+Author URI: http://tyni.uk
+*/
+
+
+
+
+/**************************************************************************
+* Function pxlfaq_enqueue_scripts()
+* Enqueues the necessary scripts for the plugin
+**************************************************************************/
+
+function bctqp_enqueue_scripts() {
+
+
+}
+
+
+add_action( 'wp_enqueue_scripts', 'bctqp_enqueue_scripts' );
+
+
+
+
+ ?>
